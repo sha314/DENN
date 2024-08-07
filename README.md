78cf6fd8cb4ba29ab0d5dc54a3334d92f1445126
shahnoor <shahnoor3pl@gmail.com>
2024-08-06 20:31:28 -0400

    rearrange

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ These codes are initially copied from my other repository sha314/APL .
 run the following command from the root directory
 $ tensorboard --logdir logs  
 you'll see a link in the terminal, open that in a browser.
+
+If you use remote ssh server then you can use '-L' option to forward the port.
+
+$ ssh user@ip -NL 6006:localhost:6006
+
+
+
+
--- src/eqn/harminic_oscillator.py
+++ src/eqn/harminic_oscillator.py
@@ -83,16 +83,103 @@ class Oscillator(APL):
     pass
 
 
+class OscillatorChaotic(Oscillator):
+    """
+    d2X_dt2 + c dX_dt + [1 + d cos(2pif t)] sin(X) = 0
+    c -> decay term
+    d -> source/force term
+    """
+    def get_classname(self):
+        a = super().get_classname()
+        a += "_Chaotic"
+        return a
+
+    # def psi_t(self, X):
+    #     """
+    #     the predictor that we will call while not training.
+    #     return : evaluated function and it's derivative. useful for phase space diagram
+    #     """
+    #     X = tf.Variable(X)
+    #     with tf.GradientTape() as g:
+    #         y = self.predict(X)
+    #         pass
+    #     dy_dX = g.gradient(y, X)
+    #     return y, dy_dX
+    
+    @tf.function
+    def pde_loss(self):
+        X = self.X_batch
+        with tf.GradientTape() as g2:
+            with tf.GradientTape() as g:
+                y = self.predict(X)
+                pass
+            dy_dX = g.gradient(y, X)
+            pass
+        # print(dy_dX)
+        d2y_dX2 = g2.gradient(dy_dX, X)
+        # print(d2y_dX2)
+        damping_term = self.ext['c']*dy_dX 
+        driven_term  = self.ext['d']*tf.cos(self.ext['2pif']*X)*tf.sin(y)
+        # print(damping_term)
+        # print(driven_term)
+        f_pde = d2y_dX2 + damping_term + driven_term + tf.sin(y)
+        loss = tf.square(f_pde)
+        loss = tf.reduce_mean(loss)
+        return loss
+    
+    
+    # @tf.function
+    # def boundary_loss(self):
+    #     y, dy_dx = self.solution_and_derivative(self.X_b_train)
+    #     #         print("x= ", x, ", y=", y)
+    #     loss = tf.square(y - self.position)
+    #     loss += tf.square(dy_dx - self.velocity)
+    #     return tf.reduce_mean(loss)
+    
+#     @tf.function
+#     def boundary_loss(self):
+#         x = tf.constant(self.lower_boundary.reshape((1,1)))
+#         with tf.GradientTape() as g:
+#             y = self.predict(x)
+#             pass
+#         dy_dx = g.gradient(y, x)
+        
+# #         print("x= ", x, ", y=", y)
+#         position = 1.
+#         velocity = 0.
+#         print(dy_dx)
+#         print(velocity)
+#         loss = tf.square(y - position)
+#         loss += tf.square(dy_dx - velocity)
+#         return tf.reduce_mean(loss)
+    
+    # @tf.function
+    # def loss_function(self):
+    #     Lp = self.pde_loss()
+    #     Lb = self.boundary_loss()*10
+    #     return Lp + Lb
+
+    pass
+    
+
 
 
 import numpy as np
 
 def test(epoch=1000,interval=500, batch_size=256):
     a = 20
-    layers = [1, a, a, 1]
 
+    # Simple Harmonic Oscillator
+    layers = [1, a, a, 1]
     model  = Oscillator(layers=layers, learning_rate=4e-4)
     ext = {'w':2}
+
+    # Or Driven oscillator
+    layers = [1, 3*a, 3*a, 3*a, 1]
+    model  = OscillatorChaotic(layers=layers, learning_rate=4e-4)
+    ext = {'w':1.,'c':0.1,'d':1.0,'2pif':2.} # Birfurcation -> two omega
+
+    
     model.set_external_parameter(ext)
     model.set_variable_order(['t'])
     y0, v0 = 1.0, 0.0
@@ -111,11 +198,14 @@ def test(epoch=1000,interval=500, batch_size=256):
     # model.set_steep_density(0.65)
     model.info()
     # model.train(epoch, interval=200, batch_size=100, generate_data=True, rar=True)
-    model.train(epoch, interval, batch_size, rar=0.4)
+    with tf.device('/GPU:0'):
+        model.train(epoch, interval, batch_size, rar=0.4)
+    pass
+    
 
     signature = model.get_classname()
     print(signature)
-    filename = signature + "_oscillator_freq{}_test".format(ext['w'])
+    filename = "./tmp/" + signature + "_oscillator_freq{}_test".format(ext['w'])
     print(filename)
 
     # from main_py.utils import directories
@@ -159,8 +249,7 @@ def test(epoch=1000,interval=500, batch_size=256):
     axs[3].ticklabel_format(axis="x", style="sci", scilimits=(0,0))
 
     # plt.show()
-    filename = "./tmp/APLv10_oscillator_high_freq{}.png".format(6)
-    plt.savefig(filename)
+    plt.savefig(filename + ".png")
 
 
 
@@ -168,5 +257,5 @@ def test(epoch=1000,interval=500, batch_size=256):
 
 
 if __name__ == "__main__":
-    test(2_000*5, 500, 128)
+    test(2_000, 500, 128)
     # test_straight_line()
